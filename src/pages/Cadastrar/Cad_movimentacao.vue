2a089ff7c816539ab07f65a73b71d2cc94be1570
PAHJunior <pahjunior@outlook.com>
2019-10-07 23:18:12 -0300

    Correção :fire:

--- src/pages/Cadastrar/Cad_movimentacao.vue
+++ src/pages/Cadastrar/Cad_movimentacao.vue
@@ -1,48 +1,37 @@
 <template>
-  <q-page class="q-pa-lg  shadow-4 ">
+  <q-page class="q-pa-lg shadow-4">
     <div class="row">
       <div class="col">
         <div class="row q-col-gutter-lg">
-
-          <div class="col-12 ">
+          <div class="col-12">
             <q-card class="transparent no-shadow">
               <q-breadcrumbs>
                 <template v-slot:separator>
-                  <q-icon
-                    size="1.2em"
-                    name="arrow_forward"
-                    color="primary"
-                  />
+                  <q-icon size="1.2em" name="arrow_forward" color="primary" />
                 </template>
 
                 <q-breadcrumbs-el icon="home" label="Home" to="/" />
-                <q-breadcrumbs-el icon="fas fa-box" to="/cadastro_movimentacao"  label="Cadastro de movimentações" />
-
+                <q-breadcrumbs-el
+                  icon="fas fa-box"
+                  to="/cadastro_movimentacao"
+                  label="Cadastro de movimentações"
+                />
               </q-breadcrumbs>
-
             </q-card>
           </div>
 
           <!-- Formulario usuario -->
           <div class="col-md-12 col-sm-12 col-xs-12 row">
-
-            <q-card class="col-12 ">
-              <q-card-section class=" q-col-gutter-sm text-center items-end">
-
-                <q-form
-                  @submit="onSubmit"
-                  @reset="onReset"
-                >
+            <q-card class="col-12">
+              <q-card-section class="q-col-gutter-sm text-center items-end">
+                <q-form @submit="onSubmit" @reset="onReset">
                   <div class="row">
                     <div class="col">
                       <div class="q-gutter-y-md row justify-center col-12">
-
                         <fieldset class="col-12 no-border">
-
                           <legend>Dados do produto</legend>
 
                           <div class="q-col-gutter-sm row">
-
                             <!-- Campo do código do produto -->
                             <q-input
                               class="col-md-3"
@@ -136,22 +125,23 @@
                               label="Descrição"
                             />
                           </div>
-
                         </fieldset>
-                        <div class="row col-md-6 ">
-                          <q-btn label="Cadastrar" @click="salvar" type="submit" color="primary" class="col-12"/>
+                        <div class="row col-md-6">
+                          <q-btn
+                            label="Cadastrar"
+                            @click="salvar"
+                            type="submit"
+                            color="primary"
+                            class="col-12"
+                          />
                         </div>
-                     </div>
+                      </div>
                     </div>
                   </div>
-
                 </q-form>
-
               </q-card-section>
             </q-card>
-
           </div>
-
         </div>
       </div>
     </div>
@@ -159,7 +149,7 @@
 </template>
 
 <script>
-import Produto from '.service/produto/produto.js'
+import Produto from '../../service/produto/produto.js'
 
 export default {
   data () {
@@ -168,7 +158,11 @@ export default {
         { label: 'Metro', value: 'm', description: 'metro - m' },
         { label: 'Litro', value: 'l', description: 'Litro - l' },
         { label: 'Quilograma', value: 'kg', description: 'Quilograma - kg' },
-        { label: 'Metro quadrado', value: 'm2', description: 'Metro quadrado - m²' },
+        {
+          label: 'Metro quadrado',
+          value: 'm2',
+          description: 'Metro quadrado - m²'
+        },
         { label: 'Metro cúbico', value: 'm3', description: 'Metro cúbico - m³' }
       ],
       produto: {
@@ -186,13 +180,11 @@ export default {
       }
     }
   },
-  computed: {
-
-  },
+  computed: {},
   methods: {
     salvar () {
       Produto.salvar(this.produto).then(resposta => {
-        alert('Dados salvos com sucesso! ')
+        alert('Dados salvos com sucesso!')
       })
     }
   }
@@ -200,11 +192,10 @@ export default {
 </script>
 
 <style>
-
 legend {
   padding: 0.2em 0.5em;
-  font-size:90%;
+  font-size: 90%;
   color: grey;
-  text-align:left;
+  text-align: left;
 }
 </style>
--- src/service/produto/produto.js
+++ src/service/produto/produto.js
@@ -2,15 +2,15 @@ import { http } from './config.js'
 
 export default {
   Produto: (produto) => {
-    return http.post('/produto', produto) //pega
+    return http.post('/produto', produto) // pega
   },
   buscarUmProduto: (id) => {
     return http.get(`/${id}`) // retorna pelo ID
   },
   buscarProduto: () => {
-    return http.get(`/`) //retorna tudo
+    return http.get(`/`) // retorna tudo
   },
-   salvar: (produto) => {
-     return http.post('produto', produto)
-   }
+  salvar: (produto) => {
+    return http.post('produto', produto)
+  }
 }
